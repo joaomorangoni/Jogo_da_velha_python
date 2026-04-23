294cdaafe7ea3bb898d2b3afe903d60f3dfe3020
vitinholas <victordeggerone@gmail.com>
2026-04-23 13:07:30 -0300

    Add files via upload

---
+++ jogo_da_velha.py
@@ -0,0 +1,15 @@
+def exibir_tabuleiro(tabuleiro):
+    print("┌───┬───┬───┐")
+    for i in range(3):
+        print(f"│ {tabuleiro[i][0]} │ {tabuleiro[i][1]} │ {tabuleiro[i][2]} │")
+        if i < 2:
+            print("├───┼───┼───┤")
+    print("└───┴───┴───┘")
+
+tabuleiro = [
+    ["X", "O", "X"],
+    [" ", "X", "O"],
+    ["O", " ", "X"]
+]
+
+exibir_tabuleiro(tabuleiro)
